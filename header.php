98b03fe7fa672f44fc6787e35eefef623659ed2e
phiexz <dhiika@phiexz.com>
2015-10-08 05:48:30 +0700

    favicon: versioning

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <!-- favicon -->
-        <link rel="icon" href="<?php echo CDN?><?php echo directory?>/favicon.ico" type="image/x-icon" sizes="16x16">
+        <link rel="icon" href="<?php echo CDN?><?php echo directory?>/favicon.ico?v=<?php echo VER?>" type="image/x-icon" sizes="16x16">
 
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
