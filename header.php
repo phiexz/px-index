95ed17d7ddfdadac42ed269040a1b538b0d9ed77
phiexz <dhiika@phiexz.com>
2015-10-08 05:45:13 +0700

    add favicon

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- favicon -->
+        <link rel="icon" href="<?php echo CDN?><?php echo directory?>/favicon.ico" type="image/x-icon" sizes="16x16">
 
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
