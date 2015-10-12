4f70d158532892f7952bcb91f8a7fa7a1642c3e8
phiexz <dhiika@phiexz.com>
2015-10-13 06:29:09 +0700

    configuration.php: beautify

--- lib/configuration.php
+++ lib/configuration.php
@@ -1,13 +1,19 @@
 <?php
-  define('CDN', '');
-  define('VER', '3.0.1');
-  //Location for px-index ex: /px-index or ../px-index
-  define('directory', '');
-  //Max File name
-  define('MaxFileName', 65);
-  //mail for contact us
-  define('ReportEmail', '');
-  //Round filesize to KB/MB/GB/TB
-  //Use this with fancyindex_exact_size = on
-  define('RoundFileSize', true)
+    // Berpengaruh ke url
+    define('CDN', '');
+    define('VER', '3.0.1');
+    
+    // Location for px-index file
+    // ex: /px-index or ../px-index or else
+    define('directory', '');
+    
+    // Max Lenght File name
+    define('MaxFileName', 65);
+    
+    // Mail for contact us
+    define('ReportEmail', '');
+    
+    //Round filesize to KB/MB/GB/TB
+    //Use this with fancyindex_exact_size = on
+    define('RoundFileSize', true)
 ?>
