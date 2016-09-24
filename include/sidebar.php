73f1bc4cc9404a5b0c749b8be028b2c050bf2090
dhiika <dhiika@phiexz.com>
2016-09-24 18:54:20 +0700

    sidebar: create menu

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,11 +1,44 @@
 <div id="sidebar" class="ui left vertical menu sidebar px-transparent">
   <a class="item">
-    Item 1
+    <div class="ui message">
+      <div class="ui centered header">Files Selection</div>
+    </div>
+    <div class="ui fluid buttons">
+      <button class="ui positive button">Select All</button>
+      <div class="or"></div>
+      <button class="ui negative button">Deselect</button>
+    </div>
   </a>
   <a class="item">
-    Item 2
+    <div class="ui message">
+      <div class="ui centered header">Site UI</div>
+    </div>
+    <div class="px-sidebar-label">Font Size</div>
+    <div class="ui fluid buttons">
+      <button class="ui button"><i class="minus icon"></i></button>
+      <button class="ui button">Default</button>
+      <button class="ui button"><i class="plus icon"></i></button>
+    </div>
+    <div class="px-sidebar-label">Icon Size</div>
+    <div class="ui fluid buttons">
+      <button class="ui button">16</button>
+      <button class="ui button">20</button>
+      <button class="ui button">24</button>
+      <button class="ui button">32</button>
+      <button class="ui button">48</button>
+    </div>
+    <div class="ui toggle checkbox px-sidebar-label">
+      <input type="checkbox" name="input-transparent-mode">
+      <label>Using Transparent</label>
+    </div>
   </a>
   <a class="item">
-    Item 3
+    <div class="ui message">
+      <div class="ui centered header">Ajax Mode</div>
+    </div>
+    <div class="ui toggle checkbox">
+      <input type="checkbox" name="input-ajax-mode">
+      <label>Using Ajax Mode</label>
+    </div>
   </a>
 </div>
