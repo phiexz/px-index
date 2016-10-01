32d3cc4c35e8c8e43288f7d665bae83fc940fd04
dhiika <dhiika@phiexz.com>
2016-10-01 23:22:57 +0700

    header: using raised segment

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <div class="px-header px-transparent">
   <div id="container-header" class="ui grid container">
     <div class="sixteen wide column centered grid px-header-info">
-      <div class="ui piled segment">
+      <div class="ui raised segment">
         <h2 class="ui center aligned icon header">
           <i class="circular inverted disk outline icon px-pointer"></i>
             <div><?php echo headerMessage; ?></div>
