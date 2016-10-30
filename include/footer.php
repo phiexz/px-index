de4417c60a2fb881e63d6db7e10fbdb0eebcf44b
phiexz <dhiika@phiexz.com>
2016-10-30 21:56:12 +0700

    footer: add backlink, version and source code link

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <div id="footer" class="ui teal inverted segment px-footer">
-  <div id="container-footer" class="ui grid container">
+  <div id="container-footer" class="ui middle aligned grid container">
     <div class="eight wide column">
-      <p>Made with love by: <b>phiexz</b></p>
+      <p>Made with love by: <a href="http://phiexz.com"><b>phiexz</b></a><br>v.<?php echo VER; ?> / <a href="github.com/phiexz/px-index">Source Code</a></p>
     </div>
     <div class="eight wide right aligned column">
       <p>Site generated in <span id="execution-time"><b></b></span> seconds</p>
