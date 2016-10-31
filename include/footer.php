551bed12cb6a32ab5fe9636b076656a8341b6957
phiexz <dhiika@phiexz.com>
2016-10-31 18:50:48 +0700

    footer: fix source code url link

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <div id="footer" class="ui inverted segment px-footer">
   <div id="container-footer" class="ui middle aligned grid container">
     <div class="eight wide column">
-      <p>Made with love by: <a href="http://phiexz.com"><b>phiexz</b></a><br>v.<?php echo VER; ?> / <a href="github.com/phiexz/px-index">Source Code</a></p>
+      <p>Made with love by: <a href="http://phiexz.com"><b>phiexz</b></a><br>v.<?php echo VER; ?> / <a href="https://github.com/phiexz/px-index">Source Code</a></p>
     </div>
     <div class="eight wide right aligned column">
       <p>Site generated in <span id="execution-time"><b></b></span> seconds</p>
